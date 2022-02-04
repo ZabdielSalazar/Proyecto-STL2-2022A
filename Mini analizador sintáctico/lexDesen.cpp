adaddd1feb255ecf2f7c41485dec095d137d7b59
ZabdielSalazar <70919441+ZabdielSalazar@users.noreply.github.com>
2022-02-04 00:33:31 -0600

    (sintactico)

---
+++ Mini analizador sintáctico/lexDesen.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+enum{ASIGNACION=257,IDENTIFICADOR};
+
+int elToken; /*global con sig. token*/
+void terminal(int token){
+if(elToken==token)
+elToken=yylex();
+else
+printf("...INFO(elToken) inesperado...%d",elToken);
+}
+void instruccion(){
+terminal(IDENTIFICADOR);
+terminal(ASIGNACION);
+expresion();
+terminal((int)';');
+}
+int main(){
+elToken=yylex();
+instruccion();
+}
+
+void termino(){
+if(elToken==IDENTIFICADOR)
+terminal(IDENTIFICADOR);
+else if(elToken=='('){
+terminal((int) '(');
+expresion();
+terminal((int) ')');
+}else
+hayError("Se esperaba un IDENTIFICADOR o '('");
+}
+
+void expresion(){
+termino();
+expresionP();
+}
+void expresionP(){
+if(elToken=='+'){
+terminal((int) '+');
+termino();expresionP();
+} /*else: corresponde a e */
+}
