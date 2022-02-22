5e7c9d18f2a7892bcde6137593d928e4faf20667
ZabdielSalazar <70919441+ZabdielSalazar@users.noreply.github.com>
2022-02-21 21:27:42 -0600

    Actualizacion analizador lexico:
    El analizador lexico ahora reúne los tokens en un archivo y delimita para prepararlo a analizador sintáctico

--- Analizador Léxico/Analizador Léxico.cpp
+++ Analizador Léxico/Analizador Léxico.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
 
 char input[200];    // Almacena la cadena de entrada 
 char token[6];      // Almacena la cadena que forma el símbolo de la palabra 
@@ -16,6 +17,10 @@ void scaner();
 
 int main()
 {
+	//Crear y abrir fichero
+	FILE* fichero;
+    fichero = fopen("compilador.txt", "wt");
+    
     p=0;
     printf("Ingresa el codigo (Termina con '#'):\n");
     
@@ -35,77 +40,108 @@ int main()
             case 8:
 				//printf("( %d,%d  )", fg,num);
 				printf("Identificador:0   "); 
+				fputs("identificador|", fichero);
 				break;
 				     
-            case -1:printf("input error\n");  break;           
+            case -1:printf("input error\n"); 
+            		fputs("error|", fichero);   
+			break; 
+			       
             default:
 				//printf("( %d,%s )", fg, token);
 				if (fg == 9){
 					printf("Entero:1   ");
+					fputs("entero|", fichero);
 				}
 				else if (fg == 5 || fg == 6 || fg == 7){
 					printf("Tipo:4   ");
+					fputs("tipo|", fichero);
 				}
 				else if (fg == 35 || fg == 10){
 					printf("OpSuma:5   ");
+					fputs("opSuma|", fichero);
 				}
 				else if (fg == 11 || fg == 12){
 					printf("OpMul:6   ");
+					fputs("opMul|", fichero);
 				}
 				else if (fg == 17 || fg == 16 || fg == 38 || fg == 39){
 					printf("OpRelac:7   ");
+					fputs("opRelac|", fichero);
 				}
 				else if (fg == 40){
 					printf("OpOr:8   ");
+					fputs("opOr|", fichero);
 				}
 				else if (fg == 42){
 					printf("OpAnd:9   ");
+					fputs("opAnd|", fichero);
 				}
 				else if (fg == 31){
 					printf("OpNot:10   ");
+					fputs("opNot|", fichero);
 				}
 				else if (fg == 30 || fg == 32){
 					printf("OpIgualdad:11   ");
+					fputs("opIgualdad|", fichero);
 				}
 				else if (fg == 19){
 					printf(";:12   ");
+					fputs(";|", fichero);
 				}
 				else if (fg == 44){
 					printf(",:13   ");
+					fputs(",|", fichero);
 				}
 				else if (fg == 20){
 					printf("(:14   ");
+					fputs("(|", fichero);
 				}
 				else if (fg == 21){
 					printf("):15   ");
+					fputs(")|", fichero);
 				}
 				else if (fg == 45){
 					printf("{:16   ");
+					fputs("{|", fichero);
 				}
 				else if (fg == 46){
 					printf("}:17   ");
+					fputs("}|", fichero);
 				}
 				else if (fg == 33){
 					printf("=:18   ");
+					fputs("=|", fichero);
 				}
 				else if (fg == 1){
 					printf("if:19   ");
+					fputs("if|", fichero);
 				}
 				else if (fg == 2){
 					printf("while:20   ");
+					fputs("while|", fichero);
 				}
 				else if (fg == 3){
 					printf("return:21   ");
+					fputs("return|", fichero);
 				}
 				else if (fg == 4){
 					printf("else:22   ");
+					fputs("else|", fichero);
 				}
 				else if (fg == 47){
 					printf("$:23   ");
+					fputs("$|", fichero);
+				}
+				else if (fg == 34){
+					printf(".:23   ");
+					fputs(".|", fichero);
 				}
         }
     } while( fg != 99 );
-
+    //cerrar fichero
+    fclose(fichero);
+	printf("\n\nTokens agregados.");
     return 0;
 }
 
